52635c77a399226d5b02cd41f54c313be10d3d8f
JongTL <62316827+JongTL@users.noreply.github.com>
2021-02-04 15:30:53 +0900

    to #2.5

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-console.log("hi");
-//changing//
+import "core-js";
+
+import express from "express";
+//const express = require('express');
+const app = express();
+
+const PORT =4000;
+
+
+const handleListening = () => console.log(`listening on:https://localhost:${PORT}`);
+const handleHome=(req,res)=> res.send("hello from home");
+const handleProfile = (req,res) =>res.send("your are on my profile");
+
+app.get("/",handleHome);
+
+app.get("/profile",handleProfile);
+
+app.listen(PORT,handleListening);
